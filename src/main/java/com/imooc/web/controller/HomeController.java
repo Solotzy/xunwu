7e42d6cff4c49505e24c1688ac12e2306befde1c
tianzeyu <498442999@qq.com>
2018-03-19 22:06:04 +0800

    error page

--- src/main/java/com/imooc/web/controller/HomeController.java
+++ src/main/java/com/imooc/web/controller/HomeController.java
@@ -13,13 +13,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("name", "慕课");
         return "index";
     }
 
-    @GetMapping("/get")
-    @ResponseBody
-    public ApiResponse get() {
-        return ApiResponse.ofMessage(200, "成功了");
+    @GetMapping("/404")
+    public String notFoundPage() {
+        return "404";
+    }
+
+    @GetMapping("/403")
+    public String accessError() {
+        return "403";
+    }
+
+    @GetMapping("/500")
+    public String internalError() {
+        return "500";
+    }
+
+    @GetMapping("/logout")
+    public String logoutPage() {
+        return "logout";
     }
 }
