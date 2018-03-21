057955793e3d15d55748754e3a42670a7c970021
tianzeyu <498442999@qq.com>
2018-03-21 21:14:03 +0800

    handler login entry point and login fail url

--- src/main/java/com/imooc/Application.java
+++ src/main/java/com/imooc/Application.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class XunwuProjectApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(XunwuProjectApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 	@GetMapping("/hello")
